9d8fce0ea9684cf94b1ee0f477d14513e198cc5b
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-05 00:12:54 +0200

    Implements JWT authentication and login handler.

--- src/main/kotlin/com/ex_dock/ex_dock/database/auth/AuthenticationVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/auth/AuthenticationVerticle.kt
@@ -1,17 +1,26 @@
 package com.ex_dock.ex_dock.database.auth
 
+import com.ex_dock.ex_dock.frontend.auth.ExDockAuthHandler
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.eventbus.EventBus
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.auth.JWTOptions
 import io.vertx.ext.auth.PubSecKeyOptions
+import io.vertx.ext.auth.authentication.UsernamePasswordCredentials
 import io.vertx.ext.auth.jwt.JWTAuth
 import io.vertx.ext.auth.jwt.JWTAuthOptions
 import java.security.KeyPair
 import java.security.KeyPairGenerator
+import java.time.Instant
 import java.util.*
 
 class AuthenticationVerticle: AbstractVerticle() {
-  private lateinit var jwtAuth: JWTAuth;
+  private lateinit var eventBus: EventBus
+  private lateinit var jwtAuth: JWTAuth
   private val generator: KeyPairGenerator = KeyPairGenerator.getInstance("RSA")
   private val keyPair: KeyPair = generator.generateKeyPair()
+  private val authHandler = ExDockAuthHandler(vertx)
 
   private companion object {
     const val BEGIN_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n"
@@ -23,6 +32,7 @@ class AuthenticationVerticle: AbstractVerticle() {
   override fun start() {
     setupJwtAuth()
 
+    eventBus = vertx.eventBus()
   }
 
   private fun setupJwtAuth() {
@@ -43,5 +53,24 @@ class AuthenticationVerticle: AbstractVerticle() {
       )
 
     jwtAuth = JWTAuth.create(vertx, config)
+  }
+
+  private fun handleLogin() {
+    eventBus.consumer<UsernamePasswordCredentials>("process.authentication.login").handler { message ->
+      val credentials = message.body()
+      authHandler.authenticate(credentials)
+    }
+  }
+
+  private fun generateAccessToken(userId: String, permissions: JsonArray): String {
+    val now: Instant = Instant.now()
+
+    val claims: JsonObject = JsonObject()
+      .put("sub", userId)
+      .put("iat", now.epochSecond)
+      .put("exp", now.plusSeconds(900).epochSecond)
+      .put("permissions", permissions)
+
+    return jwtAuth.generateToken(claims, JWTOptions().setAlgorithm("RS256"))
   }
 }
