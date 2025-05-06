d53f568e9dd828fa6bfa364a226cdaed0596fdf8
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-06 15:13:33 +0200

    ✨ feat(auth/AuthRouter.kt): Implements email-based login and uses a codec for credentials

--- src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/auth/AuthRouter.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/auth/AuthRouter.kt
@@ -2,24 +2,27 @@ package com.ex_dock.ex_dock.backend.v1.router.auth
 
 import com.ex_dock.ex_dock.backend.apiMountingPath
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.auth.authentication.UsernamePasswordCredentials
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
 
 fun Router.enableAuthRouter(vertx: Vertx, absoluteMounting: Boolean = false) {
+  val usernamePasswordCredentialsDeliveryOptions = DeliveryOptions().setCodecName("UsernamePasswordCredentialsCodec")
   val authRouter = Router.router(vertx)
   val eventBus = vertx.eventBus()
 
   authRouter.route().handler(BodyHandler.create())
 
-  authRouter["/token"].handler { ctx ->
+  authRouter.post("/token").handler { ctx ->
     val body = ctx.body().asJsonObject()
     val usernamePasswordCredentials = UsernamePasswordCredentials(
-      body.getString("username"),
+      body.getString("email"),
       body.getString("password")
     )
-    eventBus.request<String>("process.authentication.login", usernamePasswordCredentials)
+    eventBus.request<String>(
+      "process.authentication.login", usernamePasswordCredentials, usernamePasswordCredentialsDeliveryOptions)
       .onFailure { exception ->
       if (exception.message == "invalid credentials") {
         ctx.fail(401, Error("invalid credentials"))
@@ -29,7 +32,7 @@ fun Router.enableAuthRouter(vertx: Vertx, absoluteMounting: Boolean = false) {
     }.onSuccess { message ->
         ctx.response().putHeader("content-type", "application/json")
           .end(JsonObject()
-            .put("tokens", message)
+            .put("tokens", message.body())
             .encode()
           )
       }
