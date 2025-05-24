40ccd6163d162e2d9ab657c78e25dd82d268de58
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-24 23:12:00 +0200

    ``` feat(backend): Add SystemVerticle to retrieve system variables
    ```

---
+++ src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/system/SystemVerticle.kt
@@ -0,0 +1,90 @@
+package com.ex_dock.ex_dock.backend.v1.router.system
+
+import com.ex_dock.ex_dock.ClassLoaderDummy
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.eventbus.EventBus
+import io.vertx.core.eventbus.Message
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
+import java.util.Properties
+
+class SystemVerticle: AbstractVerticle() {
+  companion object {
+    val logger = KotlinLogging.logger {}
+  }
+
+  private lateinit var eventBus: EventBus
+
+  override fun start() {
+    eventBus = vertx.eventBus()
+
+    getSystemVariables()
+  }
+
+  private fun getSystemVariables() {
+    eventBus.consumer<Unit>("process.system.getVariables").handler { message ->
+      val jsonSettings = JsonObject()
+      val settingsMap = JsonArray()
+      lateinit var props: Properties
+      try {
+        props = ClassLoaderDummy::class.java.classLoader.getResourceAsStream("secret.properties").use {
+          Properties().apply { load(it) }
+        }
+
+        props.generateResponse(message, jsonSettings, settingsMap)
+      } catch (_: Exception) {
+        logger.warn { "Could not find custom settings using default settings" }
+        try {
+          props = ClassLoaderDummy::class.java.classLoader.getResourceAsStream("default.properties").use {
+            Properties().apply { load(it) }
+          }
+
+          props.generateResponse(message, jsonSettings, settingsMap)
+        } catch (_: Exception) {
+          logger.error { "Could not find default settings" }
+          message.fail(400, "Could not find settings")
+        }
+      }
+    }
+  }
+}
+
+
+private fun JsonArray.addSettingAttribute(id: String, name: String, type: String, value: Any) {
+  val setting = JsonObject()
+    .put("attribute_id", id)
+    .put("attribute_name", name.encode())
+    .put("attribute_type", type.toBackOfficeType())
+    .put("current_attribute_value", value)
+  this.add(setting)
+}
+
+private fun Properties.generateResponse(message: Message<Unit>, jsonSettings: JsonObject, settingsMap: JsonArray) {
+  this.entries.forEach { entry ->
+    val key = entry.key
+    val value = entry.value
+    settingsMap.addSettingAttribute(key.toString(), key.toString(), value::class.java.simpleName, value.toString())
+  }
+
+  jsonSettings
+    .put("block_type", "standard")
+    .put("attributes", settingsMap)
+
+  message.reply(JsonObject()
+    .put("Backend Settings", jsonSettings).encodePrettily())
+}
+
+private fun String.encode(): String {
+  var result = this.lowercase()
+  result = result.replace("_", " ")
+  result = result.replaceFirstChar { it.uppercase() }
+  return result
+}
+
+private fun String.toBackOfficeType(): String {
+  return when (this) {
+    "Int" -> "int"
+    else -> "text"
+  }
+}
