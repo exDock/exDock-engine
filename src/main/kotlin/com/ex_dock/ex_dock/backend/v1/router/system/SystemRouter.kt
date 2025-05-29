bc446e58d182ee4af58b00ff3d9f59e847ac2438
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-29 20:50:26 +0200

    ✨ feat(backend): Add `/getSettings` and `/ping` endpoints to SystemRouter.

--- src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/system/SystemRouter.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/system/SystemRouter.kt
@@ -7,12 +7,16 @@ fun Router.enableSystemRouter(vertx: Vertx) {
   val systemRouter = Router.router(vertx)
   val eventBus = vertx.eventBus()
 
-  systemRouter["/test"].handler { ctx ->
+  systemRouter["/getSettings"].handler { ctx ->
     eventBus.request<String>("process.system.getVariables", "").onFailure { error ->
       ctx.response().setStatusCode(400).end(error.message)
     }.onSuccess { message ->
       ctx.response().end(message.body())
     }
+  }
+
+  systemRouter["/ping"].handler { ctx ->
+    ctx.response().end("Server responded!")
   }
 
 
