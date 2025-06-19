21b34916bfcc0ac381d8149aa2bc981752bbfa6a
Kevin Kruijthof <kev17062004@gmail.com>
2025-06-19 14:08:18 +0200

    ``` ♻️ Refactor(SystemRouter): Improve system settings save response and add server restart message. ```

--- src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/system/SystemRouter.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/system/SystemRouter.kt
@@ -1,6 +1,8 @@
 package com.ex_dock.ex_dock.backend.v1.router.system
 
+import com.ex_dock.ex_dock.backend.v1.router.docker.ServerHealth
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.ext.web.Router
 
 fun Router.enableSystemRouter(vertx: Vertx) {
@@ -19,7 +21,9 @@ fun Router.enableSystemRouter(vertx: Vertx) {
     eventBus.request<String>("process.system.saveVariables", ctx.body().asJsonObject()).onFailure { error ->
       ctx.response().setStatusCode(400).end(error.message)
     }.onSuccess {
-      ctx.response().end()
+      eventBus.request<Unit>("process.docker.serverHealth", ServerHealth.RESTARTING, DeliveryOptions().setCodecName("ServerHealthCodec"))
+
+      ctx.response().setStatusCode(200).end("Successfully saved settings")
     }
   }
 
