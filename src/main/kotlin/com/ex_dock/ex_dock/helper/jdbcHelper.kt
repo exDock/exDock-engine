04ba86105a108e4ddefda73585056fa0d5cdcbc3
Kevin Kruijthof <kev17062004@gmail.com>
2025-07-31 10:24:47 +0200

    Updated to the router for the change in table structure

---
+++ src/main/kotlin/com/ex_dock/ex_dock/helper/jdbcHelper.kt
@@ -0,0 +1,28 @@
+package com.ex_dock.ex_dock.helper
+
+import io.vertx.core.Future
+import io.vertx.core.eventbus.DeliveryOptions
+import io.vertx.core.eventbus.Message
+import io.vertx.core.json.JsonObject
+
+val listDeliveryOptions: DeliveryOptions = DeliveryOptions().setCodecName("ListCodec")
+
+fun Future<List<JsonObject>>.replyListMessage(message: Message<String>) {
+  this.onFailure {
+    message.fail(500, it.localizedMessage)
+  }
+
+  this.onSuccess { res ->
+    message.reply(res, listDeliveryOptions)
+  }
+}
+
+fun Future<List<JsonObject>>.replySingleMessage(message: Message<String>) {
+  this.onFailure {
+    message.fail(500, it.localizedMessage)
+  }
+
+  this.onSuccess { res ->
+    message.reply(res.first())
+  }
+}
