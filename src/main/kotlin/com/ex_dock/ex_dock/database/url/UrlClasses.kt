4fd86cc1c5050e4c81521720b4cde7708c3d9092
Kevin Kruijthof <kev17062004@gmail.com>
2025-08-01 21:37:20 +0200

    feat(database): add json conversion for data classes
    Refactor data classes UrlKeys and SingleUseTemplateData to enable conversion to and from JsonObject for database storage and retrieval.
    
    - Introduce `toJson` and `fromJson` methods to `UrlKeys` and `SingleUseTemplateData` to facilitate data persistence in the database.
    - Update `UrlClasses.kt` to use `requestedId` instead of textPageId, productId, categoryId.
    - Add `toDocument` to convert data classes to `JsonObject`

--- src/main/kotlin/com/ex_dock/ex_dock/database/url/UrlClasses.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/url/UrlClasses.kt
@@ -1,69 +1,54 @@
 package com.ex_dock.ex_dock.database.url
 
-import com.ex_dock.ex_dock.database.text_pages.TextPages
+import io.vertx.core.json.JsonObject
 
-//data class UrlKeys(
-//  var urlKey: String,
-//  var upperKey: String,
-//  var pageType: PageType
-//)
-//
-//data class TextPageUrls(
-//  var urlKeys: String,
-//  var upperKey: String,
-//  var textPagesId: Int
-//)
-//
-//data class ProductUrls(
-//  var urlKeys: String,
-//  var upperKey: String,
-//  var productId: Int
-//)
-//
-//data class CategoryUrls(
-//  var urlKeys: String,
-//  var upperKey: String,
-//  var categoryId: Int
-//)
-//
-//data class FullUrlKeys(
-//  var urlKeys: UrlKeys,
-//  var textPage: TextPages?,
-//  var product: Products?,
-//  var category: Categories?
-//)
-//
-//data class JoinList(
-//  var joinTextPage: Boolean,
-//  var joinProduct: Boolean,
-//  var joinCategory: Boolean,
-//)
-//
-//data class FullUrlRequestInfo(
-//  val urlKeys: String?,
-//  val upperKey: String?,
-//  val joinList: JoinList
-//)
-//
-//enum class PageType(name: String) {
-//  PRODUCT("product"),
-//  CATEGORY("category"),
-//  TEXT_PAGE("text_page")
-//}
-//
-//fun PageType.convertToString(): String {
-//  return when (this) {
-//    PageType.TEXT_PAGE -> "text_page"
-//    PageType.CATEGORY -> "category"
-//    PageType.PRODUCT -> "product"
-//  }
-//}
-//
-//fun String.toPageType(): PageType {
-//  return when (this) {
-//    "text_page" -> PageType.TEXT_PAGE
-//    "category" -> PageType.CATEGORY
-//    "product" -> PageType.PRODUCT
-//    else -> throw IllegalArgumentException("Invalid page type: $this")
-//  }
-//}
+data class UrlKeys(
+  var urlKey: String,
+  var upperKey: String,
+  var requestedId: String,
+  var pageType: PageType
+) {
+  companion object {
+    fun fromJson(jsonObject: JsonObject): UrlKeys {
+      return UrlKeys(
+        urlKey = jsonObject.getString("_id"),
+        upperKey = jsonObject.getString("upper_key"),
+        requestedId = jsonObject.getString("requested_id"),
+        pageType = jsonObject.getString("page_type").toPageType()
+      )
+    }
+  }
+}
+
+fun UrlKeys.toDocument(): JsonObject {
+  val document = JsonObject()
+    .put("_id", this.urlKey)
+    .put("upper_key", this.upperKey)
+    .put("requested_id", this.requestedId)
+    .put("page_type", this.pageType.convertToString())
+
+  return document
+}
+
+enum class PageType(name: String) {
+  PRODUCT("product"),
+  CATEGORY("category"),
+  TEXT_PAGE("text_page")
+}
+
+fun PageType.convertToString(): String {
+  return when (this) {
+    PageType.TEXT_PAGE -> "text_page"
+    PageType.CATEGORY -> "category"
+    PageType.PRODUCT -> "product"
+  }
+}
+
+fun String.toPageType(): PageType {
+  return when (this) {
+    "text_page" -> PageType.TEXT_PAGE
+    "category" -> PageType.CATEGORY
+    "product" -> PageType.PRODUCT
+    else -> throw IllegalArgumentException("Invalid page type: $this")
+  }
+}
--- src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/template_data/single_use/SingleUseTemplateData.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/template_data/single_use/SingleUseTemplateData.kt
@@ -1,6 +1,28 @@
 package com.ex_dock.ex_dock.frontend.template_engine.template_data.single_use
 
+import io.vertx.core.json.JsonObject
+
 data class SingleUseTemplateData(
     val template: String,
     val templateData:Map<String, Any?>,
-)
+) {
+  companion object {
+    fun fromJson(json: JsonObject): SingleUseTemplateData {
+      val template = json.getString("template")
+      val templateData = json.getJsonObject("templateData").map
+
+      return SingleUseTemplateData(template, templateData)
+    }
+
+  }
+}
+
+fun SingleUseTemplateData.toDocument(): JsonObject {
+  val templateDataJson = JsonObject(this.templateData)
+
+  val document = JsonObject()
+    .put("template", this.template)
+    .put("templateData", templateDataJson)
+
+  return document
+}
