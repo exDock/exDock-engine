a730f4d4a8cab97cf57be0204d53b929c4938513
Kevin Kruijthof <kev17062004@gmail.com>
2025-08-04 11:04:00 +0200

    feat(router): add sales router
    Introduce the sales router to the backend v1 API.
    
    - Register the sales router within the main router.
    - Enable sales-related endpoints.
    
    This allows the backend to handle sales-related requests.

--- src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/BackendV1Router.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/BackendV1Router.kt
@@ -5,6 +5,7 @@ import com.ex_dock.ex_dock.backend.v1.router.auth.AuthProvider
 import com.ex_dock.ex_dock.backend.v1.router.file.initFileRouter
 import com.ex_dock.ex_dock.frontend.auth.ExDockAuthHandler
 import com.ex_dock.ex_dock.backend.v1.router.image.initImage
+import com.ex_dock.ex_dock.backend.v1.router.sales.initSalesRouter
 import com.ex_dock.ex_dock.backend.v1.router.system.enableSystemRouter
 import com.ex_dock.ex_dock.database.backend_block.BlockInfo
 import com.ex_dock.ex_dock.helper.convertJsonElement
@@ -110,6 +111,7 @@ fun Router.enableBackendV1Router(vertx: Vertx, absoluteMounting: Boolean = false
   backendV1Router.initImage(vertx)
   backendV1Router.enableSystemRouter(vertx)
   backendV1Router.initFileRouter(vertx)
+  backendV1Router.initSalesRouter(vertx)
 
   this.route(
     if (absoluteMounting) "$apiMountingPath/v1*" else "/v1*"
