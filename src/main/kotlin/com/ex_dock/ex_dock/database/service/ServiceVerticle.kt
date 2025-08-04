149e1637ae72e07f05b897927b276ed6e81e6ced
Kevin Kruijthof <kev17062004@gmail.com>
2025-08-04 11:05:40 +0200

    feat(database): seed initial admin user
    Adds an admin user to the database if one doesn't exist.
    
    - Load admin credentials from properties.
    - Upsert admin user to the 'users' collection.
    - Logs success or failure of admin user creation.

--- src/main/kotlin/com/ex_dock/ex_dock/database/service/ServiceVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/service/ServiceVerticle.kt
@@ -1,5 +1,6 @@
 package com.ex_dock.ex_dock.database.service
 
+import com.ex_dock.ex_dock.MainVerticle
 import com.ex_dock.ex_dock.database.connection.getConnection
 import com.ex_dock.ex_dock.helper.convertImage
 import io.vertx.core.Future
@@ -7,6 +8,14 @@ import io.vertx.core.VerticleBase
 import io.vertx.core.eventbus.EventBus
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.mongo.MongoClient
+
+import com.ex_dock.ex_dock.database.account.FullUser
+import com.ex_dock.ex_dock.database.account.toDocument
+import com.ex_dock.ex_dock.helper.load
+import io.vertx.ext.mongo.FindOptions
+import io.vertx.ext.mongo.UpdateOptions
+import java.util.Properties
+import org.bson.types.ObjectId
 
 class ServiceVerticle: VerticleBase() {
   private lateinit var client: MongoClient
@@ -17,8 +26,31 @@ class ServiceVerticle: VerticleBase() {
     eventBus = vertx.eventBus()
 
     imageConverter()
+    addAdminUser()
 
     return super.start()
+  }
+
+  private fun addAdminUser() {
+    val props = Properties().load()
+    val adminEmail = props.getProperty("ADMIN_EMAIL")
+    val adminPassword = props.getProperty("ADMIN_PASSWORD")
+    val adminUser = FullUser(
+      userId = ObjectId().toHexString(),
+      email = adminEmail,
+      password = adminPassword,
+      permissions = listOf(),
+      apiKey = "test"
+    )
+    val query = JsonObject().put("email", adminUser.email)
+    val update = JsonObject().put($$"$setOnInsert", adminUser.toDocument())
+    val findOptions = FindOptions()
+    val options = UpdateOptions().setUpsert(true)
+    client.findOneAndUpdateWithOptions("users", query, update, findOptions, options).onSuccess {
+      MainVerticle.logger.info { "Admin user created" }
+    }.onFailure {
+      MainVerticle.logger.error { "Failed to create admin user" }
+    }
   }
 
   private fun imageConverter() {
