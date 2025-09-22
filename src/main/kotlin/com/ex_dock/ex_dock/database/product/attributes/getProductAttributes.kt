b01aa9226ba4de429eb6bf677be4df8f452e74dc
Rens Pols <rens@cepols.nl>
2025-09-22 14:48:45 +0200

    fix(product): Get product attributes with fields
    The previous implementation was incomplete. This commit
    addresses it by:
    
    - Adding logic to retrieve product attributes.
    - Including options for specific fields to return.

--- src/main/kotlin/com/ex_dock/ex_dock/database/product/attributes/getProductAttributes.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/product/attributes/getProductAttributes.kt
@@ -1,8 +1,28 @@
 package com.ex_dock.ex_dock.database.product.attributes
 
 import io.vertx.core.eventbus.EventBus
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.mongo.FindOptions
 import io.vertx.ext.mongo.MongoClient
 
 fun EventBus.getProductAttributes(client: MongoClient) {
-  //
+  this.localConsumer<JsonObject>("process.product.attributes.getAll").handler { message ->
+    val body = message.body()
+    val filter = JsonObject()
+    val fieldsToReturn = JsonObject()
+    val findOptions = FindOptions().setFields(fieldsToReturn)
+
+    val fieldsToReturnInput: JsonArray? = body.getJsonArray("fieldsToReturn")
+
+    if (fieldsToReturnInput != null) for (fieldToReturn in fieldsToReturnInput) {
+      fieldsToReturn.put(fieldToReturn as String, 1)
+    }
+
+    client.findWithOptions("productAttributes", filter, findOptions).onFailure { err ->
+      message.fail(500, err.message)
+    }.onSuccess { res ->
+      message.reply(res)
+    }
+  }
 }
