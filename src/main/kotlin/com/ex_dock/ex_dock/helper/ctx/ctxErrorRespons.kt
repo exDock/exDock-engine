3bdfaf4d35d72a05f263cda0a63a65e382c9f616
Rens Pols <rens@cepols.nl>
2025-09-27 12:37:05 +0200

    fix(ctx): Improve error handling in RoutingContext
    The RoutingContext error handling was not comprehensive.
    
    - Add errorResponse overloads for different args
    - Log error messages for better debugging
    - Ensure correct status code and message are returned

--- src/main/kotlin/com/ex_dock/ex_dock/helper/ctx/ctxErrorRespons.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/helper/ctx/ctxErrorRespons.kt
@@ -1,8 +1,18 @@
 package com.ex_dock.ex_dock.helper.ctx
 
+import com.ex_dock.ex_dock.MainVerticle
 import com.ex_dock.ex_dock.helper.errors.failureCode
 import io.vertx.ext.web.RoutingContext
 
 fun RoutingContext.errorResponse(throwable: Throwable) {
-  this.response().setStatusCode(throwable.failureCode()).end(throwable.message)
+  this.errorResponse(throwable.failureCode(), throwable)
+}
+
+fun RoutingContext.errorResponse(failureCode: Int, throwable: Throwable) {
+  this.errorResponse(failureCode, throwable.message ?: "Missing throwable error message")
+}
+
+fun RoutingContext.errorResponse(failureCode: Int, message: String) {
+  MainVerticle.logger.error { "ctx.errorResponse() [${failureCode}] $message" }
+  this.response().setStatusCode(failureCode).end(message)
 }
