c395528b68024144f262358e4e12844520046c3a
Kevin Kruijthof <kev17062004@gmail.com>
2025-10-02 15:28:58 +0200

    fix(core): Adapt DataAccessor to AsyncExDockCache
    The DataAccessor was updated to use AsyncExDockCache
    instead of ExDockCache for improved performance.
    
    * Migrate from ExDockCache to AsyncExDockCache.
    * Update the get function to return CompletableFuture.

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/DataAccessor.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/DataAccessor.kt
@@ -7,19 +7,21 @@ import com.ex_dock.ex_dock.database.sales.Invoice
 import com.ex_dock.ex_dock.database.sales.Order
 import com.ex_dock.ex_dock.database.sales.Shipment
 import com.ex_dock.ex_dock.database.sales.Transaction
+import com.ex_dock.ex_dock.helper.AsyncExDockCache
 import com.ex_dock.ex_dock.helper.ExDockCache
+import java.util.concurrent.CompletableFuture
 
 class DataAccessor(
-  private val productCache: ExDockCache<ProductInfo>,
-  private val categoryCache: ExDockCache<CategoryInfo>,
-  private val creditMemoCache: ExDockCache<CreditMemo>,
-  private val invoiceCache: ExDockCache<Invoice>,
-  private val orderCache: ExDockCache<Order>,
-  private val shipmentCache: ExDockCache<Shipment>,
-  private val transactionCache: ExDockCache<Transaction>,
+  private val productCache: AsyncExDockCache<ProductInfo>,
+  private val categoryCache: AsyncExDockCache<CategoryInfo>,
+  private val creditMemoCache: AsyncExDockCache<CreditMemo>,
+  private val invoiceCache: AsyncExDockCache<Invoice>,
+  private val orderCache: AsyncExDockCache<Order>,
+  private val shipmentCache: AsyncExDockCache<Shipment>,
+  private val transactionCache: AsyncExDockCache<Transaction>,
 ) {
 
-  fun get(type: String, key: String): Any? {
+  fun get(type: String, key: String): CompletableFuture<*>? {
     return when (type.lowercase()) {
       "product" -> productCache.getById(key)
       "category" -> categoryCache.getById(key)
