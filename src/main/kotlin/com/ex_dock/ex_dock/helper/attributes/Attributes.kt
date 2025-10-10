7b7dfd789097f3e413782de54376ef78306ca3cd
Rens Pols <rens@cepols.nl>
2025-10-10 23:08:00 +0200

    feat(core): Add Attributes abstract class
    Adds the Attributes abstract class to define attribute handling
    in exDock. This class serves as the foundation for different
    types of attributes, providing methods for getting, setting,
    and clearing attribute values, as well as type checking. It
    also defines abstract methods for attribute creation, deletion
    and clearing all attributes.

---
+++ src/main/kotlin/com/ex_dock/ex_dock/helper/attributes/Attributes.kt
@@ -0,0 +1,159 @@
+package com.ex_dock.ex_dock.helper.attributes
+
+import com.ex_dock.ex_dock.helper.scopes.ScopeLevel
+import io.vertx.core.Future
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.mongo.FindOptions
+import io.vertx.ext.mongo.MongoClient
+import kotlin.reflect.KClass
+
+
+/**
+ * The [Attributes] abstract class dictates the way that attributes are handled in exDock.
+ * It is the basis for all different types of attributes.
+ */
+abstract class Attributes(protected val client: MongoClient) {
+  abstract val collection: String
+
+  private fun getCollectionKey(scopeKey: String): String {
+    if (scopeKey == "global") return collection
+    return "$collection-$scopeKey"
+  }
+
+  fun getAttributeValue(entityId: String, attributeKey: String, scopeKey: String): Future<Any?> {
+    return Future.future { promise ->
+      client.findOne(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        JsonObject().put(attributeKey, 1),
+      ).onFailure { err ->
+        promise.fail(err)
+      }.onSuccess { res ->
+        promise.complete(res)
+      }
+    }
+  }
+
+  fun getAttributeValue(entityIds: List<String>, attributeKey: String, scopeKey: String): Future<List<Any?>> {
+    return Future.future { promise ->
+      client.findWithOptions(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", JsonObject().put($$"$in", JsonArray(entityIds))),
+        FindOptions().setFields(JsonObject().put(attributeKey, 1)),
+      ).onFailure { err ->
+        promise.fail(err)
+      }.onSuccess { res ->
+        promise.complete(res)
+      }
+    }
+  }
+
+  fun getAttributesValue(entityId: String, attributeKeys: List<String>, scopeKey: String): Future<Any?> {
+    return Future.future { promise ->
+      val fields = JsonObject()
+      for (key in attributeKeys) fields.put(key, 1)
+      client.findOne(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        fields,
+      ).onFailure { err ->
+        promise.fail(err)
+      }.onSuccess { res ->
+        promise.complete(res)
+      }
+    }
+  }
+
+  fun getAttributesValue(entityId: String, scopeKey: String): Future<Any?> {
+    return Future.future { promise ->
+      client.findOne(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        null,
+      ).onFailure { err ->
+        promise.fail(err)
+      }.onSuccess { res ->
+        promise.complete(res)
+      }
+    }
+  }
+
+  abstract fun getAttributeType(attributeKey: String): KClass<*>
+  abstract fun checkValueType(attributeKey: String, value: Any): Boolean
+
+  fun setAttributeValue(entityId: String, attributeKey: String, value: Any, scopeKey: String): Future<Any> {
+    return Future.future { promise ->
+      if (!checkValueType(attributeKey, value)) return@future promise.fail(
+        "$value (type: ${value::class.simpleName}) is not the correct type for $attributeKey (type: ${getAttributeType(attributeKey).simpleName})")
+
+      client.findOneAndUpdate(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        JsonObject().put($$"$set", JsonObject().put(attributeKey, value)),
+      ).onFailure { err -> promise.fail(err) }.onSuccess { _ ->
+        promise.complete(value)
+      }
+    }
+  }
+  fun setAttributesValue(entityId: String, attributes: Map<String, Any>, scopeKey: String): Future<Map<String, Any>> {
+    return Future.future { promise ->
+      for ((attributeKey, value) in attributes) {
+        // TODO: save all wrong types and return it inside 1 fail()
+        if (!checkValueType(attributeKey, value)) return@future promise.fail(
+          "$value (type: ${value::class.simpleName}) is not the correct type for $attributeKey (type: ${getAttributeType(attributeKey).simpleName})")
+      }
+      client.findOneAndUpdate(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        JsonObject().put($$"$set", JsonObject(attributes)),
+      ).onFailure { err -> promise.fail(err) }.onSuccess { _ ->
+        promise.complete(attributes)
+      }
+    }
+  }
+
+  /**
+   * Clears the attribute for the entity.
+   *
+   * @throws IllegalArgumentException When you try to clear a required attribute.
+   */
+  fun clearAttributeValue(entityId: String, attributeKey: String, scopeKey: String): Future<Unit> {
+    return Future.future { promise ->
+      client.findOneAndUpdate(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        JsonObject().put($$"$unset", JsonObject().put(attributeKey, null)),
+      ).onFailure { err -> promise.fail(err) }.onSuccess { _ -> promise.complete() }
+    }
+  }
+  /**
+   * Clears the attributes for the entity.
+   *
+   * @throws IllegalArgumentException When you try to clear a required attribute.
+   */
+  fun clearAttributesValue(entityId: String, attributeKeys: List<String>, scopeKey: String): Future<Unit> {
+    val attributes = mutableMapOf<String, Any?>()
+    for (key in attributeKeys) attributes[key] = null
+
+    return Future.future { promise ->
+      client.findOneAndUpdate(
+        getCollectionKey(scopeKey),
+        JsonObject().put("_id", entityId),
+        JsonObject().put($$"$unset", JsonObject(attributes)),
+      ).onFailure { err -> promise.fail(err) }.onSuccess { _ -> promise.complete() }
+    }
+  }
+  /**
+   * Clears all the attributes for the entity. This is meant as an assist for the removal of the entityId
+   *
+   * Attention: Also removes the required attributes!
+   */
+  abstract fun clearAllAttributesValue(entityId: String)
+
+  abstract fun createAttribute(attributeName: String, attributeKey: String, dataType: String, scopeLevel: ScopeLevel)
+
+  // TODO: fun editAttribute()
+
+  abstract fun deleteAttribute(attributeKey: String)
+}
