d020bb4268138987288c5e7ae52fbd75886b2d7c
Rens Pols <rens@cepols.nl>
2025-10-20 15:17:03 +0200

    feat(futures): Add Future.onSuccess(promise) extension function
    Adds onSuccess method to complement onFailure for promise completion.

--- src/main/kotlin/com/ex_dock/ex_dock/helper/futures/future_handle_parent_future.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/helper/futures/future_handle_parent_future.kt
@@ -3,8 +3,20 @@ package com.ex_dock.ex_dock.helper.futures
 import io.vertx.core.Future
 import io.vertx.core.Promise
 
+/**
+ * This onFailure method replaces the standard onFailure { promise.fail(it) }
+ */
 fun <T> Future<T>.onFailure(promise: Promise<*>): Future<T> {
   return this.onFailure { err ->
     promise.fail(err)
+  }
+}
+
+/**
+ * This onSuccess method replaces the standard onSuccess { promise.complete(it) }
+ */
+fun <T> Future<T>.onSuccess(promise: Promise<T>): Future<T> {
+  return this.onSuccess { res ->
+    promise.complete(res)
   }
 }
