40f0607bf657d7ef585ee8db9ee6f8e7d30c4652
Kevin Kruijthof <kev17062004@gmail.com>
2025-11-14 14:49:06 +0100

    fix(frontend): Improve page rendering
    - Fetch and render dynamic content
    - Handle missing URL parameters
    - Fix error handling for URL requests

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/router/frontendRouter.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/router/frontendRouter.kt
@@ -1,42 +1,20 @@
 package com.ex_dock.ex_dock.frontend.router
 
 import com.ex_dock.ex_dock.backend.v1.router.auth.AuthProvider
-import com.ex_dock.ex_dock.backend.v1.router.auth.enableAuthRouter
-import com.ex_dock.ex_dock.backend.v1.router.docker.initDocker
-import com.ex_dock.ex_dock.backend.v1.router.enableBackendV1Router
-import com.ex_dock.ex_dock.backend.v1.router.image.initOpenImageRouter
-import com.ex_dock.ex_dock.backend.v1.router.websocket.initWebsocket
-import com.ex_dock.ex_dock.helper.codecs.registerGenericCodec
+import com.ex_dock.ex_dock.database.url.UrlKeys
 import io.github.oshai.kotlinlogging.KLogger
+import io.vertx.core.Future
 import io.vertx.core.Vertx
 import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.core.http.HttpMethod
-import io.vertx.ext.auth.PubSecKeyOptions
-import io.vertx.ext.auth.jwt.JWTAuth
-import io.vertx.ext.auth.jwt.JWTAuthOptions
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.CorsHandler
-import io.vertx.ext.web.handler.JWTAuthHandler
 
-fun Router.enableFrontendRouter(vertx: Vertx, logger: KLogger) {
+fun Router.enableFrontendRouter(vertx: Vertx, logger: KLogger, authProvider: AuthProvider) {
   val frontendRouter: Router = Router.router(vertx)
   val pairDeliveryOptions = DeliveryOptions().setCodecName("PairCodec")
   val eventBus = vertx.eventBus()
-  val authProvider = AuthProvider()
-  val jwtAuth = JWTAuth.create(
-    vertx,
-    JWTAuthOptions()
-      .addPubSecKey(
-        PubSecKeyOptions()
-          .setAlgorithm("RS256")
-          .setBuffer(authProvider.publicKey)
-      )
-      .addPubSecKey(
-        PubSecKeyOptions()
-          .setAlgorithm("RS256")
-          .setBuffer(authProvider.privateKey)
-      )
-  )
 
   eventBus.send(
     "process.authentication.registerKeys",
@@ -44,14 +22,60 @@ fun Router.enableFrontendRouter(vertx: Vertx, logger: KLogger) {
     pairDeliveryOptions
   )
 
-  frontendRouter.route().handler(CorsHandler.create()
-    .allowedMethod(HttpMethod.OPTIONS)
-    .allowedMethod(HttpMethod.GET)
-    .allowedMethod(HttpMethod.POST)
+  frontendRouter.route().handler(
+    CorsHandler.create()
+      .allowedMethod(HttpMethod.OPTIONS)
+      .allowedMethod(HttpMethod.GET)
+      .allowedMethod(HttpMethod.POST)
   )
 
   frontendRouter.get("/about").handler { ctx ->
-    ctx.end("about page for the APIs")
+    ctx.end("about page for the Frontend")
+  }
+
+  frontendRouter["/*"].handler { ctx ->
+    val params = ctx.queryParams()
+    val fullUrl = ctx.request().path()
+
+    eventBus.request<JsonObject>("process.url.getUrlByKey", fullUrl).onFailure {
+      ctx.fail(404, Error("Page not found"))
+      return@onFailure
+    }.onSuccess { fetchedUrl ->
+      val urlObject: UrlKeys = UrlKeys.fromJson(fetchedUrl.body())
+      val request = JsonObject()
+
+      for (param in urlObject.requiredParameters) {
+        request.put(param, params.get(param))
+        if (!params.contains(param)) {
+          ctx.fail(400, Error("Missing required parameter: $param"))
+          return@onSuccess
+        }
+      }
+
+      val futures = mutableListOf<Future<Unit>>()
+      var pageCode = ""
+      urlObject.templates.forEach { templateId ->
+        futures.add(
+          Future.future { promise ->
+            val newBody = JsonObject(request.toString()).put("template_key", templateId)
+            eventBus.request<JsonObject>("template.generate.compiled", newBody).onFailure {
+              promise.fail(it.message)
+            }.onSuccess {
+              pageCode += it.body()
+              promise.complete()
+            }
+          }
+        )
+      }
+
+      Future.all<Unit>(futures).onFailure {
+        ctx.fail(500, it)
+      }.onSuccess { _ ->
+        ctx.response()
+          .putHeader("Content-Type", "text/html")
+          .end(pageCode)
+      }
+    }
   }
 
   this.route().subRouter(frontendRouter)
