163fcc2868665ae2fa6a287936d51f048cae7743
Kevin Kruijthof <kev17062004@gmail.com>
2025-11-14 14:50:20 +0100

    fix(url): Refactor URL creation and update
    The request body was not correctly parsed when creating/updating URLs, causing errors.
    
    - Update URL creation/update endpoints to correctly parse the request body using UrlKeys.fromJson().
    - Use UrlKeys codec for event bus communication.
    - Return updated UrlKeys object as document

--- src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/url/UrlRouter.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/backend/v1/router/url/UrlRouter.kt
@@ -1,13 +1,18 @@
 package com.ex_dock.ex_dock.backend.v1.router.url
 
 import com.ex_dock.ex_dock.MainVerticle
+import com.ex_dock.ex_dock.database.url.UrlKeys
+import com.ex_dock.ex_dock.database.url.toDocument
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 
 fun Router.initUrlRouter(vertx: Vertx) {
   val urlRouter = Router.router(vertx)
   val eventBus = vertx.eventBus()
+  val urlDeliveryOptions = DeliveryOptions().setCodecName("UrlKeysCodec")
+
 
   urlRouter["/getAll"].handler { ctx ->
     eventBus.request<List<JsonObject>>("process.url.getAllUrlKeys", "").onFailure {
@@ -29,24 +34,25 @@ fun Router.initUrlRouter(vertx: Vertx) {
   }
 
   urlRouter.post("/create").handler { ctx ->
-    val body = ctx.body().asJsonObject()
-    eventBus.request<JsonObject>("process.url.createUrlKey", body).onFailure {
+    val requestBody = ctx.body().asJsonObject()
+    val body = UrlKeys.fromJson(requestBody)
+    eventBus.request<UrlKeys>("process.url.createUrlKey", body, urlDeliveryOptions).onFailure {
       ctx.fail(500, it)
       MainVerticle.logger.error { it.localizedMessage }
     }.onSuccess {
       ctx.response().putHeader("Content-Type", "application/json")
-        .end(it.body().encode())
+        .end(it.body().toDocument().encode())
     }
   }
 
   urlRouter.put("/update").handler { ctx ->
-    val body = ctx.body().asJsonObject()
-    eventBus.request<JsonObject>("process.url.updateUrlKey", body).onFailure {
+    val body = UrlKeys.fromJson(ctx.body().asJsonObject())
+    eventBus.request<UrlKeys>("process.url.updateUrlKey", body).onFailure {
       ctx.fail(500, it)
       MainVerticle.logger.error { it.localizedMessage }
     }.onSuccess {
       ctx.response().putHeader("Content-Type", "application/json")
-        .end(it.body().encode())
+        .end(it.body().toDocument().encode())
     }
   }
 
